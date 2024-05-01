8e134c049cd8f7badc22b9e4ca96681d5b111041
Hanan574 <hannimuhammad574@gmail.com>
2024-05-01 18:38:24 +0100

    add partials and basic CSS

--- app.js
+++ app.js
@@ -13,41 +13,44 @@ app.set("views", "view");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 app.get("/afar", (req, res) => {
-  res.render("afar");
+  res.render("afar", { title: "Afar" });
 });
 app.get("/amhara", (req, res) => {
-  res.render("amhara");
+  res.render("amhara", { title: "Amhara" });
 });
 app.get("/gambella", (req, res) => {
-  res.render("gambella");
+  res.render("gambella", { title: "Gambella" });
 });
 app.get("/gurage", (req, res) => {
-  res.render("gurage");
+  res.render("gurage", { title: "Gurage" });
 });
 app.get("/oromo", (req, res) => {
-  res.render("oromo");
+  res.render("oromo", { title: "Oromo" });
 });
 app.get("/sidama", (req, res) => {
-  res.render("sidama");
+  res.render("sidama", { title: "sidama" });
 });
 app.get("/snnpr", (req, res) => {
-  res.render("snnpr");
+  res.render("snnpr", { title: "snnpr" });
 });
 app.get("/somalia", (req, res) => {
-  res.render("somalia");
+  res.render("somalia", { title: "somalia" });
 });
 app.get("/wolayta", (req, res) => {
-  res.render("wolayta");
+  res.render("wolayta", { title: "wolayta" });
 });
 app.get("/tigray", (req, res) => {
-  res.render("tigray");
+  res.render("tigray", { title: "tigray" });
 });
 app.get("/contribute", (req, res) => {
-  res.render("contribute");
+  res.render("contribute", { title: "contribute" });
 });
 app.get("/explore", (req, res) => {
-  res.render("explore");
+  res.render("explore", { title: "explore" });
+});
+app.get("/login", (req, res) => {
+  res.render("login", { title: "login" });
 });
